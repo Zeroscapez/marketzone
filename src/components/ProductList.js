872af9e8f92cb02e1c0079cc75a1272015b989ec
Zeroscapez <ultlomini@gmail.com>
2023-11-14 19:04:05 -0500

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -25,7 +25,7 @@ class ProductList extends Component {
   }
 
   fetchProducts() {
-    axios.get('http://marketzonedb.ctnruybewt4o.us-east-2.rds.amazonaws.com/marketzone/api/products')
+    axios.get('https://marketzone-api.onrender.com/marketzone/api/products')
       .then((response) => {
         this.setState({ products: response.data.data });
       })
