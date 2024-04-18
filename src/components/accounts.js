e475be4395b1a8e7f63e43440f1e8a709b82938e
Zeroscapez <ultlomini@gmail.com>
2024-04-18 01:29:07 -0400

    Update accounts.js

--- src/components/accounts.js
+++ src/components/accounts.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import OrderDetails from './OrderDetails'; // Import the new component
-import  '../css/account.css'
+import '../css/account.css'
 
 function Accounts() {
   const [user, setUser] = useState({
     first_name: '',
     last_name: '',
     username: '',
+    reward_points: 0,
   });
 
   const [newPassword, setNewPassword] = useState('');
@@ -49,7 +50,7 @@ function Accounts() {
       axios.post('https://marketzone-api.vercel.app/marketzone/api/resetPassword', { newPassword }, {
         headers: {
           Authorization: `Bearer ${token}`,
-          
+
         },
       })
         .then((response) => {
@@ -73,9 +74,9 @@ function Accounts() {
         name: productName,
         description: productDescription,
         price: productPrice,
-        quantity: productQuantity, 
+        quantity: productQuantity,
       };
-  
+
       axios.post('https://marketzone-api.vercel.app/marketzone/api/listProducts', productData, {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -93,72 +94,71 @@ function Accounts() {
         });
     }
   };
-  
+
   const [productQuantity, setProductQuantity] = useState(1);
 
   return (
-    
+
     <div>
       <header className='navbar' >
-      <a href='/'><h1>Marketzone</h1></a>
+        <a href='/'><h1>Marketzone</h1></a>
       </header>
       <div className='info-container'>
-      <div className='contentwithin'>
-      <h1>Account Details</h1>
-      <div>
-        <p>Full Name: {user.first_name} {user.last_name}</p>
-        <p>Username: {user.username}</p>
-        <p>Email: {user.email}</p>
-      </div>
-      
-      <h2>List a Product</h2>
-      <div className='product-form'>
-        
-      <input
-  className='forminputs'
-  type="text"
-  placeholder="Image Link"
-  value={productImage}
-  onChange={(e) => setProductImage(e.target.value)}
-/>
+        <div className='contentwithin'>
+          <h1>Account Details</h1>
+          <div>
+            <p>Full Name: {user.first_name} {user.last_name}</p>
+            <p>Username: {user.username}</p>
+            <p>Email: {user.email}</p>
+            <p>Reward Points: {user.reward_points}</p> {/* Display reward points here */}
+          </div>
 
-        <input
-        className='forminputs'
-          type="text"
-          placeholder="Product Name"
-          value={productName}
-          onChange={(e) => setProductName(e.target.value)}
-        />
-        <input
-        className='forminputs'
-          type="text"
-          placeholder="Product Description"
-          value={productDescription}
-          onChange={(e) => setProductDescription(e.target.value)}
-        />
-        <input
-        className='forminputs'
-          type="number"
-          placeholder="Product Price"
-          value={productPrice}
-          onChange={(e) => setProductPrice(e.target.value)}
-        />
-        <input
-        className='forminputs'
-          type="number"
-          placeholder="Product Quantity"
-          value={productQuantity}
-          onChange={(e) => setProductQuantity(e.target.value)}
-        />
-        
-        <button onClick={handleProductSubmit}>List Product</button>
-      </div>
+          <h2>List a Product</h2>
+          <div className='product-form'>
+            <input
+              className='forminputs'
+              type="text"
+              placeholder="Image Link"
+              value={productImage}
+              onChange={(e) => setProductImage(e.target.value)}
+            />
+            <input
+              className='forminputs'
+              type="text"
+              placeholder="Product Name"
+              value={productName}
+              onChange={(e) => setProductName(e.target.value)}
+            />
+            <input
+              className='forminputs'
+              type="text"
+              placeholder="Product Description"
+              value={productDescription}
+              onChange={(e) => setProductDescription(e.target.value)}
+            />
+            <input
+              className='forminputs'
+              type="number"
+              placeholder="Product Price"
+              value={productPrice}
+              onChange={(e) => setProductPrice(e.target.value)}
+            />
+            <input
+              className='forminputs'
+              type="number"
+              placeholder="Product Quantity"
+              value={productQuantity}
+              onChange={(e) => setProductQuantity(e.target.value)}
+            />
+            <button onClick={handleProductSubmit}>List Product</button>
+          </div>
 
-      {/* New component to display order details */}
-      <OrderDetails />
-      </div>
+          {/* New component to display order details */}
+          <OrderDetails />
+        </div>
       </div>
     </div>
+
   );
 }
 
